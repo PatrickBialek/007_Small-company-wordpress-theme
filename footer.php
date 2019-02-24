bf94e8cd197a839a5cae140e3b8f6f0a401ee8ec
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-02-24 22:11:59 +0100

    small changes in css

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
 <footer class="footer">
+
 <?php 
 
 include get_template_directory() . '/template-parts/6-footer/_1-footer-content.php';
 
 ?>
+
 </footer>
+
 <?php wp_footer(); ?>
--- page-o-nas.php
+++ page-o-nas.php
@@ -1,13 +1,17 @@
 <?php get_header(); ?>
+
 <main class="main">
-	<?php 
 
-	include get_template_directory() . '/template-parts/4-about-us/_1-heading.php';
+<?php 
 
-	include get_template_directory() . '/template-parts/4-about-us/_2-basic-info.php';
+include get_template_directory() . '/template-parts/4-about-us/_1-heading.php';
 
-	include get_template_directory() . '/template-parts/4-about-us/_3-services.php';
+include get_template_directory() . '/template-parts/4-about-us/_2-basic-info.php';
 
-	?>
+include get_template_directory() . '/template-parts/4-about-us/_3-services.php';
+
+?>
+
 </main>
+
 <?php get_footer(); ?>
