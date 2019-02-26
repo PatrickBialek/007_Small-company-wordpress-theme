19f107b19f7cd184f64209ee063ec90217fd2eaf
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-02-26 14:16:37 +0100

    working on opinions section

--- template-parts/2-front-page/_4-opinions.php
+++ template-parts/2-front-page/_4-opinions.php
@@ -1,7 +1,30 @@
-<section class="opinions">
-	<div class="opinions__content">
-		<span class="opinions__quotension-mark">"</span>
-		<p class="opinions__quote">Nam efficitur neque elementum facilisis tempor. Vivamus in finibus ante, eget maximus diam. In nec ante sit amet sapien malesuada dignissim. Pellentesque id nisl non dolor dapibus ullamcorper et quis nulla.</p>
-		<h4 class="opinions__author">Marek Czarniecki<h4>
-	</div>
-</section>
+<?php
+
+if( have_rows('opinions_section') ): 
+
+	while( have_rows('opinions_section') ): the_row(); ?>
+
+	<section class="opinions">
+
+		<?php
+
+		if( have_rows('opinions_single') ): 
+
+			while( have_rows('opinions_single') ): the_row(); 
+			
+			$author = get_sub_field('opinions_author');
+			$opnion = get_sub_field('opinions_quote');
+			
+			?>
+
+			<div class="opinions__single">
+				<span class="opinions__quotension-mark">"</span>
+				<p class="opinions__quote"><?php echo $opnion ?></p>
+				<h4 class="opinions__author"><?php echo $author ?><h4>
+			</div>
+
+		<?php endwhile; endif; ?>	
+
+	</section>
+
+<?php endwhile; endif; ?>	
