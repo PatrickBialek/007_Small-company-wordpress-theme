49c6ed1eafa3f6dd676518ad01f2f8790131b712
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-02-27 16:30:51 +0100

    Fixed heading about us section title bug

--- template-parts/4-about-us/_1-heading.php
+++ template-parts/4-about-us/_1-heading.php
@@ -1,7 +1,7 @@
 <section class="heading-section" style="background-image: url('<?php echo get_theme_file_uri( '/images/about-us-background.jpg' ); ?>');">
 	<div class="heading-section__content">
 		<div class="heading-section__text">
-			<h1 class="heading-primary"><?php the_field('about_us__heading');?></h1>
+			<h1 class="heading-primary"><?php the_field('about_us_heading');?></h1>
 			<h2 class="heading-secondary"><?php the_field('about_us_sub_heading');?></p>
 		</div>
 	</div>
