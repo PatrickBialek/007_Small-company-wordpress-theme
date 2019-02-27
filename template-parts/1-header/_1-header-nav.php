8b34c307935e8abdb710ad105fd16719e4a931df
Patrick Bialek <patrykkrzysztofbialek@gmail.com>
2019-02-27 04:39:30 +0100

    working on contact form

--- template-parts/1-header/_1-header-nav.php
+++ template-parts/1-header/_1-header-nav.php
@@ -3,11 +3,13 @@
 		<h2>Es-Daw</h2>
 	</div>
 	<nav class="header__nav">
+
 	<?php wp_nav_menu( [ 
 		'theme_location' => 'main-menu',
 		'container'=> false,
 		'items_wrap' => '<ul>%3$s</ul>'
 	] ); ?>
+	
 	</nav>
 	<div class="header__hamburger-container" id="hamburger-icon">
 		<span></span>
--- template-parts/5-contact/_3-content.php
+++ template-parts/5-contact/_3-content.php
@@ -1,46 +1,49 @@
 <section class="contact">
 	<div class="contact__content">
 		<div class="contact__info-box">
+
 			<?php if( get_field('contact_title') ): ?>
 				<h2 class="heading-secondary"><?php the_field('contact_title'); ?></h2>
-			<?php endif; ?>
-			<?php if( get_field('contact_description') ): ?>
+			<?php endif;
+
+			if( get_field('contact_description') ): ?>
 				<p class="paragraph paragraph--dark"><?php the_field('contact_description'); ?></p>
 			<?php endif; ?>
+
 			<div class="contact__contact-data">
+
 				<?php if( get_field('contact_adres_1') ): ?>
 					<span><?php the_field('contact_adres_1'); ?></span>
-				<?php endif; ?>
-				<?php if( get_field('contact_adres_2') ): ?>
+				<?php endif;
+
+				if( get_field('contact_adres_2') ): ?>
 					<span><?php the_field('contact_adres_2'); ?></span>
-				<?php endif; ?>
-				<?php if( get_field('contact_mail') ): ?>
+				<?php endif;
+
+				if( get_field('contact_mail') ): ?>
 					<span><?php the_field('contact_mail'); ?></span>
-				<?php endif; ?>
-				<?php if( get_field('contact_phone_1') ): ?>
+				<?php endif;
+
+				if( get_field('contact_phone_1') ): ?>
 					<span><?php the_field('contact_phone_1'); ?></span>
-				<?php endif; ?>
-				<?php if( get_field('contact_phone_2') ): ?>
+				<?php endif;
+
+				if( get_field('contact_phone_2') ): ?>
 					<span><?php the_field('contact_phone_2'); ?></span>
 				<?php endif; ?>
+
 			</div>
 		</div>
+
 		<div class="contact__form-box">
-			<form>
-				<div class="contact__form-row">
-					<input class="contact__text contact__text--50" type="text" id="name-field" name="name" placeholder="imię i nazwisko">
-					<input class="contact__text contact__text--50" type="text" id="phone-filed" name="phone" placeholder="numer telefonu">
-				</div>
-				<div class="contact__form-row">
-					<input class="contact__text contact__text--100" type="text" id="mail-field" name="mail" placeholder="adres e-mail">	
-				</div>
-				<div class="contact__form-row">
-					<textarea class="contact__textarea" id="message-field" name="message" placeholder="Treść wiadmości..."></textarea>
-				</div>
-				<div class="contact__form-row">
-					<input class="contact__send" type="submit" value="Wyślij">
-				</div>
-			</form>
+			<?php 
+
+				$short_code = get_field('contact_form');
+			
+				echo do_shortcode( $short_code ); 
+			
+			?>
 		</div>
+		
 	</div>
 </section>
